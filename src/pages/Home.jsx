3eae4b8f28920d8b17ac4725692242e18535886d
Nicholas Wease <skynet60@outlook.com>
2019-05-08 13:24:08 +0200

    first commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Header from '../components/home/Header';
+
+const Home = () => {
+    return (
+        <React.Fragment>
+            <Header />
+        </React.Fragment>
+    );
+};
+
+export default Home;
